ed4c7f3f117a27f6cbd691bee762e5e601f35b8f
EscolanoA <escolanoallan4@gmail.com>
2025-01-16 14:15:11 +0100

    sort testing

--- script.js
+++ script.js
@@ -1,9 +1,72 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
-let listOfCards = document.getElementsByClassName("card");
 
-for (let i = 0; i < listOfCards.length; i++) {
-    listOfCards[i].style.animation = `opacityanim 1s cubic-bezier(.22,.53,.51,.82) forwards`;
-    listOfCards[i].style.animationDelay = `${i * 0.3}s`; // Stagger start time by 0.5s for each card
+
+let servicescontainer = document.getElementById("servicescontainer")
+
+let tailles = document.getElementById("taillescontainer")
+let abattage = document.getElementById("abattagecontainer")
+let haubanage = document.getElementById("haubanagecontainer")
+let broyage = document.getElementById("broyagecontainer")
+let entretien = document.getElementById("entretiencontainer")
+
+let navbartailles = document.getElementById("tailles")
+let navbarabattage = document.getElementById("abattage")
+let navbarhaubanage = document.getElementById("haubanage")
+let navbarbroyage = document.getElementById("broyage")
+let navbarentretien = document.getElementById("entretien")
+
+animatecards()
+
+function animatecards() {
+    let listOfCards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < listOfCards.length; i++) {
+        listOfCards[i].style.animation = `opacityanim 1s cubic-bezier(.22,.53,.51,.82) forwards`;
+        listOfCards[i].style.animationDelay = `${i * 0.3}s`;
+    }
+}
+
+function sort(servicetype) {
+    if (servicetype == "tailles") {
+        servicescontainer.innerHTML = tailles.outerHTML + abattage.outerHTML + haubanage.outerHTML + broyage.outerHTML + entretien.outerHTML
+        animatecards()
+    }
+
+    if (servicetype == "abattage") {
+        servicescontainer.innerHTML = abattage.outerHTML + tailles.outerHTML + haubanage.outerHTML + broyage.outerHTML + entretien.outerHTML
+        animatecards()
+    }
+
+    if (servicetype == "haubanage") {
+        servicescontainer.innerHTML = haubanage.outerHTML + tailles.outerHTML + abattage.outerHTML + broyage.outerHTML + entretien.outerHTML
+        animatecards()
+    }
+
+    if (servicetype == "broyage") {
+        servicescontainer.innerHTML = broyage.outerHTML + tailles.outerHTML + abattage.outerHTML + haubanage.outerHTML + entretien.outerHTML
+        animatecards()
+    }
+
+    if (servicetype == "entretien") {
+        servicescontainer.innerHTML = entretien.outerHTML + tailles.outerHTML + abattage.outerHTML + haubanage.outerHTML + broyage.outerHTML
+        animatecards()
+    }
+}
+
+navbartailles.addEventListener("click", function(){sort("tailles")})
+navbarabattage.addEventListener("click", function(){sort("abattage")})
+navbarhaubanage.addEventListener("click", function(){sort("haubanage")})
+navbarbroyage.addEventListener("click", function(){sort("broyage")})
+navbarentretien.addEventListener("click", function(){sort("entretien")})
+
+
+function animatecards() {
+    let listOfCards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < listOfCards.length; i++) {
+        listOfCards[i].style.animation = `opacityanim 1s cubic-bezier(.22,.53,.51,.82) forwards`;
+        listOfCards[i].style.animationDelay = `${i * 0.3}s`;
+    }
 }
